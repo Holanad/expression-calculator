6c9602628684712dac7cee96bbb51d0131cf9c47
volkov <volkov99.at@gmail.com>
2021-08-30 09:30:05 +0300

    feat: add result task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+  
 function eval() {
     // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let count = 0;
+    expr = expr.split(' ').join('');
+
+    for(let i=0; i<expr.length; i++){
+        if(expr[i] === '(') count++;
+        if(expr[i] === ')') count--;
+    }
+    if(count != 0) throw Error('ExpressionError: Brackets must be paired!');
+    if(expr.includes('/0')) throw Error('TypeError: Division by zero.');
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
